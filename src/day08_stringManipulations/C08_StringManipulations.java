db7a45f30f87bc85eae5c629fba05f06496f63cb
bulutluoz <abbulutluoz@gmail.com>
2023-09-07 11:42:26 +0200

    8.gun sonu

--- src/day08_stringManipulations/C08_StringManipulations.java
+++ src/day08_stringManipulations/C08_StringManipulations.java
@@ -1,5 +1,7 @@
 package day08_stringManipulations;
 
+import java.util.Scanner;
+
 public class C08_StringManipulations {
     public static void main(String[] args) {
 
@@ -8,5 +10,25 @@ public class C08_StringManipulations {
         //         String’in uzunlugu tek sayi ise ortadaki harfi silin ve yerine :( yazdirin.
 
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen bir metin giriniz");
+        String girilenMetin = scanner.nextLine();
+
+        int uzunluk = girilenMetin.length();
+        String yeniHal ="";
+
+        if (uzunluk % 2 == 0){ // uzunluk cift ise
+
+            yeniHal = girilenMetin.substring(0,uzunluk/2) +
+                      ":)"+
+                      girilenMetin.substring(uzunluk/2);
+
+        }else { // uzunluk tek ise
+            yeniHal = girilenMetin.substring(0,uzunluk/2)+
+                      ":(" +
+                      girilenMetin.substring((uzunluk+1)/2);
+        }
+
+        System.out.println(girilenMetin+ " yazdiniz ve ben onu degistirdim : " +yeniHal);
     }
 }
