5cda0e873d43fa3432851fa61ce89466308d4c19
bulutluoz <abbulutluoz@gmail.com>
2023-10-10 10:26:12 +0200

    24.gun sonu

---
+++ src/day24_inheritance_constructorCall/C01_Inheritance.java
@@ -0,0 +1,10 @@
+package day24_inheritance_constructorCall;
+
+public class C01_Inheritance {
+
+    public static void main(String[] args) {
+
+        C01_Inheritance obj = new C01_Inheritance();
+        obj.toString();
+    }
+}
