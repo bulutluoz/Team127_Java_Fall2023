81c975e5f1d8471dce2983a3882f0efe75b1e20b
bulutluoz <abbulutluoz@gmail.com>
2023-10-11 10:04:57 +0200

    24.gun sonu

---
+++ src/day25_inheritanceDataTypeKullanimi_overriding/B_Kuslar.java
@@ -0,0 +1,8 @@
+package day25_inheritanceDataTypeKullanimi_overriding;
+
+public class B_Kuslar {
+    String kanat= "kanatlidirlar";
+    String solunum= "akcigerle nefes alirlar";
+    String gaga= "gagalari vardir";
+    String cogalma= "yumurtayla cogalirlar";
+}
---
+++ src/day25_inheritanceDataTypeKullanimi_overriding/C_AvciKuslar.java
@@ -0,0 +1,9 @@
+package day25_inheritanceDataTypeKullanimi_overriding;
+
+public class C_AvciKuslar {
+
+    String hareket= "ucarlar";
+    String beslenme= "et yerler";
+    String pence= "pencelidir";
+    String gaga= "sivri gagali";
+}
